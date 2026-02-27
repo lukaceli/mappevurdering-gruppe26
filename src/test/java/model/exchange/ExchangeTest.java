cc2f4ba195bcd7bfc0e13f00b4d06d97b27e3e6b
noahmn <Noahmn@stud.ntnu.no>
2026-02-27 16:42:42 +0100

    fix test

--- src/test/java/model/exchange/ExchangeTest.java
+++ src/test/java/model/exchange/ExchangeTest.java
@@ -155,8 +155,8 @@ class ExchangeTest {
     for (int i = 0; i < 5000; i++) {
       BigDecimal change = exchange.getRandomPercentChange();
       // Sjekk at den ikke er større enn 0.153 eller mindre enn -0.15
-      assertTrue(change.compareTo(new BigDecimal("-0.15")) >= 0, "Change too low: " + change);
-      assertTrue(change.compareTo(new BigDecimal("0.153")) <= 0, "Change too high: " + change);
+      assertTrue(change.compareTo(exchange.biggestPriceChange.negate()) >= 0, "Change too low: " + change);
+      assertTrue(change.compareTo(exchange.biggestPriceChange.add(exchange.bonusPriceGain)) <= 0, "Change too high: " + change);
     }
   }
 
