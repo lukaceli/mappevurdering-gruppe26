7073c91c086d643605178676f1850b771f7bb6e6
noahmn <Noahmn@stud.ntnu.no>
2026-03-09 12:47:11 +0100

    added javafx

--- src/main/java/app/GUI.java
+++ src/main/java/app/GUI.java
@@ -1,16 +1,26 @@
 package app;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-  public static void main(String[] args) {
-    launch(args);
+  @Override
+  public void start(Stage stage) {
+    Label label = new Label("Hello");
+    label.setLayoutX(100);
+    label.setLayoutY(100);
+
+    Scene scene = new Scene(label, 400, 300);
+
+    stage.setTitle("Aksje Spill");
+    stage.setScene(scene);
+    stage.show();
   }
 
-  @Override
-  public void start(Stage primaryStage) {
-
+  void main(String[] args) {
+    launch();
   }
 }
