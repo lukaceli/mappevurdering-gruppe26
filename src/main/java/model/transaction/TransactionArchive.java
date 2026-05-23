a8ec4a9ee7dab212a5eadfec2bd14c72d9f56db9
noahmn <Noahmn@stud.ntnu.no>
2026-05-23 18:25:35 +0200

    wrote javadoc and fixed checkstyle for following classes: all in transaction package

--- src/main/java/model/transaction/TransactionArchive.java
+++ src/main/java/model/transaction/TransactionArchive.java
@@ -3,15 +3,26 @@ package model.transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores committed transactions for a player.
+ */
 public class TransactionArchive {
+
   private final List<Transaction> transactions;
 
-
+  /**
+   * Constructs an empty {@code TransactionArchive}.
+   */
   public TransactionArchive() {
     this.transactions = new ArrayList<>();
-
   }
 
+  /**
+   * Adds a transaction to the archive if it has been committed.
+   *
+   * @param transaction the transaction to add
+   * @return {@code true} if the transaction was added, {@code false} if not committed
+   */
   public boolean add(Transaction transaction) {
     if (transaction.isCommitted()) {
       transactions.add(transaction);
@@ -20,53 +31,33 @@ public class TransactionArchive {
     return false;
   }
 
+  /**
+   * Returns whether the archive contains no transactions.
+   *
+   * @return {@code true} if the archive is empty, {@code false} otherwise
+   */
   public boolean isEmpty() {
     return transactions.isEmpty();
   }
 
-  public List<Transaction> getTransactions(int week) {
-    List<Transaction> transactionsByWeek = new ArrayList<>();
-
-    for (Transaction transAc : transactions) {
-      if (transAc.getWeek() == week) {
-        transactionsByWeek.add(transAc);
-      }
-    }
-    return transactionsByWeek;
-  }
-
-  public List<Purchase> getPurchases(int week) {
-    List<Purchase> purchaseList = new ArrayList<>();
-
-    for (Transaction transaction : transactions) {
-      if (transaction.getWeek() == week && transaction instanceof Purchase) {
-        purchaseList.add((Purchase) transaction);
-      }
-    }
-    return purchaseList;
-  }
-
-  public List<Sale> getSales(int week) {
-    List<Sale> salesList = new ArrayList<>();
-    for (Transaction transaction : transactions) {
-      if (transaction.getWeek() == week && transaction instanceof Sale) {
-        salesList.add((Sale) transaction);
-      }
-    }
-    return salesList;
-  }
-
-
-
+  /**
+   * Returns a copy of all transactions in the archive.
+   *
+   * @return a list of all {@link Transaction} objects
+   */
   public List<Transaction> getAll() {
     return new ArrayList<>(transactions);
   }
 
+  /**
+   * Returns the number of distinct weeks in which transactions occurred.
+   *
+   * @return the count of distinct weeks
+   */
   public int countDistinctWeeks() {
     return (int) transactions.stream()
-        .map(Transaction::getWeek)
-        .distinct()
-        .count();
+            .map(Transaction::getWeek)
+            .distinct()
+            .count();
   }
-
 }
--- src/main/java/model/transaction/TransactionFactory.java
+++ src/main/java/model/transaction/TransactionFactory.java
@@ -4,11 +4,32 @@ import model.calculator.PurchaseCalculator;
 import model.calculator.SaleCalculator;
 import model.stock.Share;
 
+/**
+ * Factory class for creating {@link Transaction} instances.
+ */
 public class TransactionFactory {
+
+  private TransactionFactory() {
+  }
+
+  /**
+   * Creates a new uncommitted {@link Purchase} transaction.
+   *
+   * @param share the share to purchase
+   * @param week  the week in which the purchase occurs
+   * @return a new {@link Purchase} transaction
+   */
   public static Purchase createPurchase(Share share, int week) {
     return new Purchase(share, week, new PurchaseCalculator(share));
   }
 
+  /**
+   * Creates a new uncommitted {@link Sale} transaction.
+   *
+   * @param share the share to sell
+   * @param week  the week in which the sale occurs
+   * @return a new {@link Sale} transaction
+   */
   public static Transaction createSale(Share share, int week) {
     return new Sale(share, week, new SaleCalculator(share));
   }
